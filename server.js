9696db32ccbe2652ecc06413125764e592647040
mdharb <md20.harb21@gmail.com>
2021-08-09 21:23:38 +0300

    lab12 final touches

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ const PORT = process.env.PORT;
 const JWKSURI = process.env.JWKSURI;
 const MONGO_DB_URL = process.env.MONGO_DB_URL;
 
-// const { bestBook } = require('./controllers/Books');
 const { seedUsersCollection } = require('./models/User');
 const {User} = require('./models/User');
 
